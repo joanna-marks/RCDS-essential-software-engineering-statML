7c3dd7aa329272be1edeae0e2b7a203aea651faf
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2025-03-03 14:35:23 +0000

    feat: add and run pivotsort

---
+++ sort.py
@@ -0,0 +1,7 @@
+def pivot_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    left = [x for x in arr[1:] if x < pivot]
+    right = [x for x in arr[1:] if x >= pivot]
+    return pivot_sort(left) + [pivot] + pivot_sort(right)
